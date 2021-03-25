4347dfb85f06a4de346840d14b93cabc526a0d9b
wjwebdevelopments <wjwebdevelopments@outlook.com>
2021-03-25 12:29:43 -0400

    Create product findById and use select() and populate()

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,11 @@
-const { Product } = require('../models');
+const { Product,Category } = require('../models');
 
 exports.getProducts = async (req, res) => {
     try {
-        const productList = await Product.find();
+        const productList = await Product.find()
+            .select('name image category -_id') // REVIEW: select()
+            .populate('category', 'name -_id'); // REVIEW: populate()
+
         if (productList.length == 0) {
             return res.status(404).json({
                 success: false,
@@ -11,7 +14,9 @@ exports.getProducts = async (req, res) => {
         }
         res.json({
             succes: true,
-            productList
+            data: {
+                products: productList
+            }
         });
     } catch (err) {
         res.status(500).json({
@@ -21,23 +26,110 @@ exports.getProducts = async (req, res) => {
     }
 }
 
-exports.postProducts = (req, res) => {
+exports.getProductById = async (req, res) => {
+
+    const product = await Product.findById(req.params._id)
+        .populate('category');
+
+    if (!product) 
+        return res.status(404).json({
+            succes: false,
+            msg: 'the product with the given ID does not exist'
+        });
+    
+    res.json({
+        succes: true,
+        data: {
+            product
+        }
+    })
+}
+
+exports.postProducts = async (req, res) => {
+
+    const category = await Category.findById(req.body.category);
+    if (!category) 
+        return res.status(400).json({
+            success: false,
+            msg: 'Invalid category!'
+        });
+
     const product = new Product({
         name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
         image: req.body.image,
-        countInStock: req.body.countInStock
+        images: req.body.images,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeature: req.body.isFeature,
+        dateCreated: req.body.dateCreated
     });
-    product.save()
-        .then((createdProduct => {
-            res.status(201).json({
-                succes: true,
-                product: createdProduct
-            });
-        }))
-        .catch(err => {
-            res.status(500).json({
-                succes: false,
-                error: err
-            });
+
+    const newProduct = await product.save();
+
+    if (!newProduct)
+        return res.status(500).json({
+            success: false,
+            msg: 'The product cannot be created!'
         });
+    
+    res.json({
+        success: true,
+        product: newProduct
+    });
+    
+}
+
+exports.updateProduct = async (req, res) => {
+    const _id = req.params['_id'];
+
+    const category = await Category.findById(req.body.category);
+    if (!category) 
+        return res.status(400).json({
+            success: false,
+            msg: 'Invalid category!'
+        });
+
+    try {
+
+        const product = await Product.findByIdAndUpdate(_id, {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            image: req.body.image,
+            images: req.body.images,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeature: req.body.isFeature,
+            dateCreated: req.body.dateCreated
+        }, {new: true});
+
+        if ( !product ) {
+            return res.status(500).json({ 
+                succes: false, 
+                msg: 'the product cannot be updated!' 
+            });
+        }
+
+        res.json({
+            succes: true,
+            msg: 'The product has been successfully updated!',
+            data: { product }
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            succes: false,
+            error: err
+        });
+    }
 }
--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,15 @@ const { Router } = require('express');
 const router = Router();
 
 const { 
-    getProducts, 
-    postProducts 
+    getProducts,
+    getProductById, 
+    postProducts,
+    updateProduct 
 } = require('../controllers/products');
 
-router.get('/', getProducts)
+router.get('/', getProducts);
+router.get('/:_id', getProductById);
 router.post('/', postProducts);
+router.put('/:_id', updateProduct);
 
 module.exports = router;
