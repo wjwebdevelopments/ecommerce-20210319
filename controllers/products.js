6c50ba750fe29cce5bbec3e5fbb886a39709f476
wjwebdevelopments <wjwebdevelopments@outlook.com>
2021-03-25 22:25:10 -0400

    Complete product page

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,15 @@
 const { Product,Category } = require('../models');
+const { isValidObjectId } = require('mongoose');
 
 exports.getProducts = async (req, res) => {
+
+    let filter = {}
+    if (req.query.categories) {
+        filter = { category: req.query.categories.split(',') }
+    }
+
     try {
-        const productList = await Product.find()
+        const productList = await Product.find(filter)
             .select('name image category -_id') // REVIEW: select()
             .populate('category', 'name -_id'); // REVIEW: populate()
 
@@ -88,7 +95,15 @@ exports.postProducts = async (req, res) => {
 exports.updateProduct = async (req, res) => {
     const _id = req.params['_id'];
 
+    if ( !isValidObjectId(_id) ) 
+        return res.status(400).json({
+            success: false,
+            msg: 'Invalid product id!'
+        });
+    
+
     const category = await Category.findById(req.body.category);
+
     if (!category) 
         return res.status(400).json({
             success: false,
@@ -129,6 +144,77 @@ exports.updateProduct = async (req, res) => {
     } catch (err) {
         res.status(500).json({
             succes: false,
+            msg: '500: Internal server error',
+            error: err
+        });
+    }
+}
+
+exports.deleteProduct = async (req, res) => {
+
+    const _id = req.params['_id'];
+
+    try {
+        const product = await Product.findByIdAndRemove(_id);
+
+        if (!product) 
+            return res.status(500).json({
+                success: false,
+                msg: 'The product cannot be deleted!'
+            });
+
+        res.json({
+            succes: true,
+            msg: 'The product has been successfully deleted!'
+        })
+    } catch(err) {
+        res.status(500).json({
+            succes: false,
+            msg: '500: Internal server error',
+            error: err
+        });
+    }
+}
+
+exports.getProductsCount = async (req, res) => {
+
+    try {
+        const productsCount = await Product.countDocuments((count) => count);
+        res.json({
+            succes: true,
+            data: {
+                count: productsCount
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            succes: false,
+            msg: '500: Internal server error',
+            error: err
+        });
+    }
+}
+
+exports.getProductsFeature = async (req, res) => {
+
+    let count = req.params.count ? req.params.count : 0;
+    count = parseInt(count);
+
+    try {
+
+        const products = await Product.find({ isFeature: true }).limit(count);
+
+        res.json({
+            succes: true,
+            data: {
+                products
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            succes: false,
+            msg: '500: Internal server error',
             error: err
         });
     }
--- routes/products.js
+++ routes/products.js
@@ -5,12 +5,18 @@ const {
     getProducts,
     getProductById, 
     postProducts,
-    updateProduct 
+    updateProduct,
+    deleteProduct,
+    getProductsCount,
+    getProductsFeature 
 } = require('../controllers/products');
 
 router.get('/', getProducts);
 router.get('/:_id', getProductById);
 router.post('/', postProducts);
 router.put('/:_id', updateProduct);
+router.delete('/:_id', deleteProduct);
+router.get('/get/count', getProductsCount);
+router.get('/get/feature/:count', getProductsFeature);
 
 module.exports = router;
